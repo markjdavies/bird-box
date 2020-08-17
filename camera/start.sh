2f710b60529209ea23935acaf86b7931264ac34d
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-08-17 23:07:18 +0100

    log message

--- camera/start.sh
+++ camera/start.sh
@@ -5,5 +5,6 @@ then
     cd /data
     python -m http.server 80
 else
+    echo Starting YouTube stream
     raspivid -o - -t 0 -w 1280 -h 720 -fps 25 -b 4000000 -g 50 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
 fi
