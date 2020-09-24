82d4b88b9ed7171dff8c846084128c46cc46959d
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-09-24 21:21:30 +0100

    Fix timestamp warning (#2)
    * Parameterise raspivid args
    
    * null audio stream
    
    * Parameterise with 'works for me' defaults

--- camera/start.sh
+++ camera/start.sh
@@ -5,6 +5,43 @@ then
     cd /data
     python -m http.server 80
 else
+    echo Exposure settings: br: ${BRIGHTNESS:=70} contrast: ${CONTRAST:=75} ISO: ${ISO:=800} ev: ${EV:=0}
+    echo Region of interest: $ROI
     echo Starting YouTube stream
-    raspivid -o - -t 0 -w 1280 -h 720 -fps 25 -b 4000000 -g 50 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
+    raspivid -o - -t 0 \
+            -w ${WIDTH:=1280} \
+            -h ${HEIGHT:=720} \
+            -fps ${FPS:=25} \
+            -b ${BITRATE:=8000000} \
+            -g ${INTRA:=50} \
+            --brightness $BRIGHTNESS \
+            --contrast $CONTRAST \
+            --ISO $ISO \
+            --ev $EV \
+            --exposure ${EXPOSURE:=night} \
+            --awb ${AWB:=greyworld} \
+            --rotation ${ROTATION:=0} \
+            --roi ${ROI:=0,0,1,1} \
+        | \
+        ffmpeg \
+            -fflags +genpts \
+            -f lavfi -i anullsrc=r=48000:cl=mono \
+            -re \
+            -ar 44100 \
+            -ac 2 \
+            -acodec pcm_s16le \
+            -f s16le \
+            -ac 2 \
+            -i /dev/zero \
+            -f h264 \
+            -thread_queue_size ${THREAD_QUEUE_SIZE:=512} \
+            -i - \
+            -vcodec \
+        copy \
+            -acodec aac \
+            -ab 128k \
+            -g 50 \
+            -strict experimental \
+            -f \
+        flv rtmp://a.rtmp.youtube.com/live2/$YOUTUBE_KEY
 fi
