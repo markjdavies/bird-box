19fb482ee2a27c6b2e2594e2da094452327bff49
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-02-09 23:29:51 +0000

    Handle error from creat-broadcast

--- camera/start.sh
+++ camera/start.sh
@@ -15,47 +15,55 @@ do
             --privacy-status "public"  \
             --stream-title "Nesting Box Stream" \
             --description "Oxfordshire, UK")
+        errorMessage=$(echo streamData | jq '.error.message')
+        if [ -z "$errorMessage" ]
+            echo 'Success'
+        else
+            echo $errorMessage
+            echo Trying default stream
+            streamId=${YOU_TUBE_API_KEY}
+        fi
     else
         streamId=$(echo streamData | jq '.streamId')
-        secondsRemaining=$(echo streamData | jq '.secondsRemaining')
-        millisecondsRemaining=$((secondsRemaining * 1000))
-
-        echo Starting YouTube stream $streamId for $((secondsRemaining / 60)) minutes
-        echo Exposure settings: br: ${BRIGHTNESS:=70} contrast: ${CONTRAST:=75} ISO: ${ISO:=800} ev: ${EV:=0}
-        echo Region of interest: $ROI
-        echo Starting YouTube stream
-        raspivid -o - -t $millisecondsRemaining \
-            -n \
-            -ih \
-            -w ${WIDTH:=1280} \
-            -h ${HEIGHT:=720} \
-            -fps ${FPS:=24} \
-            -b ${BITRATE:=3000000} \
-            -g ${INTRA:=48} \
-            --brightness $BRIGHTNESS \
-            --contrast $CONTRAST \
-            --ISO $ISO \
-            --ev $EV \
-            --exposure ${EXPOSURE:=night} \
-            --awb ${AWB:=greyworld} \
-            --rotation ${ROTATION:=0} \
-            --roi ${ROI:=0,0,1,1} \
-        | \
-        ffmpeg -re \
-            -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
-            -re \
-            -f h264 \
-            -thread_queue_size ${THREAD_QUEUE_SIZE:=1024} \
-            -i - \
-            -vcodec copy \
-            -acodec aac \
-            -ab 128k \
-            -g 50 \
-            -strict normal \
-            -t $secondsRemaining \
-            -f flv rtmp://a.rtmp.youtube.com/live2/$streamId
-        echo Streaming finished
     fi
+    secondsRemaining=$(echo streamData | jq '.secondsRemaining')
+    millisecondsRemaining=$((secondsRemaining * 1000))
+
+    echo Starting YouTube stream $streamId for $((secondsRemaining / 60)) minutes
+    echo Exposure settings: br: ${BRIGHTNESS:=70} contrast: ${CONTRAST:=75} ISO: ${ISO:=800} ev: ${EV:=0}
+    echo Region of interest: $ROI
+    echo Starting YouTube stream
+    raspivid -o - -t $millisecondsRemaining \
+        -n \
+        -ih \
+        -w ${WIDTH:=1280} \
+        -h ${HEIGHT:=720} \
+        -fps ${FPS:=24} \
+        -b ${BITRATE:=3000000} \
+        -g ${INTRA:=48} \
+        --brightness $BRIGHTNESS \
+        --contrast $CONTRAST \
+        --ISO $ISO \
+        --ev $EV \
+        --exposure ${EXPOSURE:=night} \
+        --awb ${AWB:=greyworld} \
+        --rotation ${ROTATION:=0} \
+        --roi ${ROI:=0,0,1,1} \
+    | \
+    ffmpeg -re \
+        -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
+        -re \
+        -f h264 \
+        -thread_queue_size ${THREAD_QUEUE_SIZE:=1024} \
+        -i - \
+        -vcodec copy \
+        -acodec aac \
+        -ab 128k \
+        -g 50 \
+        -strict normal \
+        -t $secondsRemaining \
+        -f flv rtmp://a.rtmp.youtube.com/live2/$streamId
+    echo Streaming finished
 done
 
 #  \
