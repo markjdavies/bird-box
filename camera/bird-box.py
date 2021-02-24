b243c3e3d7536451c032adb63023585fe00cb104
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-02-24 13:03:34 +0000

    good grief - only take 1 successful picture

--- camera/bird-box.py
+++ camera/bird-box.py
@@ -5,24 +5,23 @@ import picamera
 from datetime import datetime
 
 
-for i in range(100):
-  for attempt in range(10):
-    try:
-      with picamera.PiCamera() as camera:
-        picamera.PiCamera.CAPTURE_TIMEOUT = 60 # seconds
-        camera.resolution = (1280, 720)
-        # Camera warm-up time
-        time.sleep(2)
-        filename = datetime.now().strftime('/pics/birdbox_%Y%m%d%H%M%S.jpg')
-        camera.capture(filename)
-        camera.close()
-    except Exception as e:
-      print('Could not take still picture')
-      print(e)
-      time.sleep(10)
-    else:
-      print('Picture taken')
-      time.sleep(10)
-      break
+for attempt in range(100):
+  try:
+    with picamera.PiCamera() as camera:
+      picamera.PiCamera.CAPTURE_TIMEOUT = 60 # seconds
+      camera.resolution = (1280, 720)
+      # Camera warm-up time
+      time.sleep(2)
+      filename = datetime.now().strftime('/pics/birdbox_%Y%m%d%H%M%S.jpg')
+      camera.capture(filename)
+      camera.close()
+  except Exception as e:
+    print('Could not take still picture')
+    print(e)
+    time.sleep(10)
   else:
-      print('Giving up on still picture')
+    print('Picture taken')
+    time.sleep(10)
+    break
+else:
+    print('Giving up on still picture')
