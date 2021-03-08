86fbf3559cb90a224ddc4e2b1fb2f1b60a83124b
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-03-08 22:51:29 +0000

    Make still retry times easier

--- camera/bird-box.py
+++ camera/bird-box.py
@@ -6,11 +6,11 @@ import picamera
 from datetime import datetime
 
 
-for attempt in range(100):
+for attempt in range(300):
   filename = datetime.now().strftime('/pics/birdbox_%Y%m%d%H%M%S.jpg')
   try:
     with picamera.PiCamera() as camera:
-      picamera.PiCamera.CAPTURE_TIMEOUT = 60 # seconds
+      picamera.PiCamera.CAPTURE_TIMEOUT = 53 # seconds
       camera.resolution = (1280, 720)
       camera.awb_mode = 'off'
       camera.start_preview()
