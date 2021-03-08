b5e93d5eafd1564f951166ad84720029d77bf375
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-03-08 22:12:43 +0000

    stills tweaks (awb off, delete empty file)

--- camera/bird-box.py
+++ camera/bird-box.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python
 
+import os
 import time
 import picamera
 from datetime import datetime
 
 
 for attempt in range(100):
+  filename = datetime.now().strftime('/pics/birdbox_%Y%m%d%H%M%S.jpg')
   try:
     with picamera.PiCamera() as camera:
       picamera.PiCamera.CAPTURE_TIMEOUT = 60 # seconds
       camera.resolution = (1280, 720)
+      camera.awb_mode = 'off'
+      camera.start_preview()
       # Camera warm-up time
       time.sleep(2)
-      filename = datetime.now().strftime('/pics/birdbox_%Y%m%d%H%M%S.jpg')
       camera.capture(filename)
       camera.close()
   except Exception as e:
+    os.remove(filename)
     print('Could not take still picture')
     print(e)
-    time.sleep(10)
+    time.sleep(5)
   else:
     print('Picture taken')
-    time.sleep(10)
     break
 else:
     print('Giving up on still picture')
