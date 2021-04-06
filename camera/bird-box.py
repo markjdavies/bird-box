0de260cec828942d9469f378a16b8cbbfdaf9115
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-04-06 19:40:08 +0100

    Maintain 'latest' image as well as counted images

--- camera/bird-box.py
+++ camera/bird-box.py
@@ -4,31 +4,33 @@ import os
 import time
 import picamera
 from datetime import datetime
+from shutil import copyfile
 
 while True:
-  filename = datetime.now().strftime('/pics/birdbox_%Y%m%d%H%M%S.jpg')
+  filename = '/pics/birdbox_latest.jpg'
   try:
     with picamera.PiCamera() as camera:
       picamera.PiCamera.CAPTURE_TIMEOUT = 20 # seconds
       camera.resolution = (1280, 720)
       camera.iso = 800
       camera.awb_mode = 'off'
-      camera.awb_gains = (1, 1)
-      # camera.awb_mode = 'off'
+      camera.awb_gains = (0.95, 0.95)
       camera.start_preview()
       # Camera warm-up time
       time.sleep(3)
       camera.capture(filename)
       camera.close()
   except Exception as e:
-    try:
-      print('Could not take still picture')
-      print(e)
-      os.remove(filename)
-    except Exception as e2:
-      print('Could not remove file')
-      print(e)
+    print('Could not take still picture')
+    print(e)
     time.sleep(5)
   else:
     print('Picture taken')
+    dated_filename = datetime.now().strftime('/pics/birdbox_%Y%m%d%H%M%S.jpg')
+    try:
+      copyfile(filename, dated_filename)
+    except IOError as e:
+      print("Unable to copy file. %s" % e)
+    except:
+      print("Unexpected error")
     break
