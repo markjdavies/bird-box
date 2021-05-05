96eb10cec2904902d7d37ac0ea4de470a24d61f0
markjdavies <46246921+markjdavies@users.noreply.github.com>
2021-05-05 13:17:26 +0100

    sensor_mode not sensorMode

--- camera/bird-box.py
+++ camera/bird-box.py
@@ -13,7 +13,9 @@ while True:
     with picamera.PiCamera() as camera:
       # Timeout must be referenced on class not the instance, due to this bug: https://github.com/waveform80/picamera/issues/329
       picamera.PiCamera.CAPTURE_TIMEOUT = int(os.environ.get('STILL_CAPTURE_TIMEOUT', '60')) # seconds
-      camera.sensorMode = int(os.environ.get('STILL_SENSOR_MODE', '3'))
+      # set sensor_mode twice, to be sure: https://picamera.readthedocs.io/en/release-1.13/api_camera.html#picamera.PiCamera.sensor_mode
+      camera.sensor_mode = int(os.environ.get('STILL_SENSOR_MODE', '3'))
+      camera.sensor_mode = int(os.environ.get('STILL_SENSOR_MODE', '3'))
       camera.resolution = make_tuple(os.environ.get('STILL_RESOLUTION', '(3280, 2464)'))
       camera.exposure_mode = 'sports'
       camera.iso = 0
